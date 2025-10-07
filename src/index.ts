f00d2ddee0d25ff524ced8277249f01815b8d763
Jacek Nowak <jaceknowak226@gmail.com>
2025-10-07 22:53:15 +0200

    styles: format project

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
 export { createLogger, type Logger, type LoggerOptions } from './core';
-export {
-  TimeFormat,
-  defaultFormatter,
-  jsonFormatter,
-  compactFormatter,
-  type FormatterFunction,
-} from './formatters';
+export { TimeFormat, defaultFormatter, jsonFormatter, compactFormatter, type FormatterFunction } from './formatters';
 export { LogLevel } from './levels';
